afbbdba205291f3dde48b317116aa9aaecb674f1
ZacharyLawrence13 <39809819+ZacharyLawrence13@users.noreply.github.com>
2018-10-11 17:24:39 -0400

    gamemanagerfixed

--- 2D Sidescroller/Assets/Scripts/GameManager.cs
+++ 2D Sidescroller/Assets/Scripts/GameManager.cs
@@ -4,37 +4,27 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour {
 
-    public GameObject[] worldList[];
+    public GameObject world1;
+    public GameObject world2;
 
-    private int currentWorldIndex
+    public int worldCounter = 2;
 
 	void Start () {
-        currentWorldIndex = 0;
-        for (int i = 1; i < )
+        
 	}
 	
 	void Update () {
-		if (Input.GetKeyDown(KeyCode.LeftShift))    // When left shift is pressed
+
+        if (Input.GetKeyDown(KeyCode.LeftShift))    // When left shift is pressed
         {
-            Debug.Log("Left Shift Pressed");
-            if (World1.activeSelf == true)  // if world 1 is active
+            worldCounter++;
+            if(worldCounter % 2 == 0)
             {
-                Debug.Log("World1 is active");
-                World1.SetActive(false);    // set world 1 to false
-                Debug.Log("World1 is set as false");
-                World2.SetActive(true);     // set world 2 to true
-                Debug.Log("World2 is set as true");
-                world1JustActivated = false;
+                world1.SetActive(true);
             }
-            
-            if (World2.activeSelf == true)
+            else
             {
-                Debug.Log("World2 is active");
-                World2.SetActive(false);
-                Debug.Log("World2 is set as false");
-                World1.SetActive(true);
-                Debug.Log("World1 is set as true");
-                return;
+                world2.SetActive(true);
             }
         }
     }
