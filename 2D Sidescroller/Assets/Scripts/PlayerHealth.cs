38f41c4f568cd86a70ea22550a7eb1c49be4b9f7
Tony Matos <matosman1209@gmail.com>
2018-10-20 18:46:19 -0400

    Yup

---
+++ 2D Sidescroller/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public bool inDeadzone;
+
+    public float playerHealth;
+
+
+    IEnumerator DoDeadzoneDamage(float damageDuration, int damageCount, float damageAmount)
+    {
+        inDeadzone = true;
+        int currentCount = 0;
+        while(currentCount < damageCount)
+        {
+            playerHealth -= damageAmount;
+            yield return new WaitForSeconds(damageDuration);
+            currentCount++;
+        }
+        inDeadzone = false;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.tag == "Deadzone" && inDeadzone)
+        {
+            StartCoroutine(DoDeadzoneDamage(1.0f, 5, 1.0f));
+        }
+    }
+}
